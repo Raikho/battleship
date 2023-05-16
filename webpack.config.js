86e15e0d6560609feee633368a2f210fb7e3ce53
Mateen <zahidx297@yahoo.com>
2023-05-15 20:50:18 -0400

    add basic boilerplate

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
     mode: 'development',
@@ -7,5 +8,12 @@ module.exports = {
         filename: '[name].js',
         path: path.resolve(__dirname, 'dist'),
         clean: true,
-    }
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            title: 'Battleship',
+            filename: 'index.html',
+            template: 'src/template.html'
+        }),
+    ],
 }
