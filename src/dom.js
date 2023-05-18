078a9dcdcb5681993ff311fd24d7a3199e351e61
Mateen <zahidx297@yahoo.com>
2023-05-18 02:17:15 -0400

    create dom update board functionality

--- src/dom.js
+++ src/dom.js
@@ -1,12 +1,28 @@
 const DOM = {};
 
 DOM.createBoard = function(parent) {
-    for (let x = 0; x < 10; x++) {
-        for (let y = 0; y < 10; y++) {
+    for (let y = 0; y < 10; y++) {
+        for (let x = 0; x < 10; x++) {
             let node = createDiv(parent);
             node.dataset.x = x;
             node.dataset.y = y;
         }
+    }
+}
+
+DOM.updateBoard = function(parent, board) {
+    for (let ship of board.ships) {
+        for (let segment of ship.segments) {
+            let node = parent.querySelector(
+                `[data-x="${segment.x}"][data-y="${segment.y}"]`);
+            node.classList.add('ship');
+        }
+    }
+    for (let hit of board.hits) {
+        let node = parent.querySelector(
+            `[data-x="${hit.x}"][data-y="${hit.y}"]`);
+        node.classList.add('hit');
+        node.textContent = 'x';
     }
 }
 
--- src/index.js
+++ src/index.js
@@ -5,10 +5,31 @@ import Player from './player.js';
 import DOM from './dom.js';
 
 console.log('DOM: ', DOM); // debug
-let boardNode = document.querySelector('.board');
+let p1BoardNode = document.querySelector('.board.player1');
+let p2BoardNode = document.querySelector('.board.player2');
+DOM.createBoard(p1BoardNode);
+DOM.createBoard(p2BoardNode);
 
 let player1 = new Player();
 let player2 = new Player();
+
+player1.board.addShip(1, 1, 2);
+player1.board.addShip(3, 1, 2);
+player1.board.addShip(5, 1, 2);
+player1.board.addShip(7, 1, 3);
+player1.board.addShip(9, 1, 4);
+
+player2.board.addShip(0, 1, 2);
+player2.board.addShip(2, 1, 2);
+player2.board.addShip(4, 1, 3);
+player2.board.addShip(6, 1, 4);
+player2.board.addShip(8, 1, 2);
+
+player1.board.receiveAttack(1, 2);
+player2.board.receiveAttack(7, 4);
+
+DOM.updateBoard(p1BoardNode, player1.board);
+DOM.updateBoard(p2BoardNode, player2.board);
 
 // GAMEMODE STATES
 // game-type-selection
