edb570c1516c8ef69dbb860638b87672d33633c0
Mateen <zahidx297@yahoo.com>
2023-05-18 02:48:28 -0400

    add response objects to gameboard events

--- src/gameboard.js
+++ src/gameboard.js
@@ -7,34 +7,47 @@ export default class Gameboard {
     }
 
     addShip(x, y, length = 1, shape = 'vertical') {
-        if (isOutOfBounds(x, y)) return;
+        if (isOutOfBounds(x, y))
+            return {status: 'failure', message: 'ship out of bounds'};
         
         let ship = new Ship(x, y, length, shape);
 
         for (let segment of ship.segments)
             if (isOutOfBounds(segment.x, segment.y))
-                return;
+                return {status: 'failure', message: 'ship extends out of bounds'};
 
         for (let segment1 of ship.segments)
             for (let ship2 of this.ships)
                 for (let segment2 of ship2.segments)
                     if (segment1.x == segment2.x && segment1.y == segment2.y)
-                        return;
+                        return {status: 'failure', message: 'ship intersects other ship'};
 
         this.ships.push(ship);
+        return {status: 'success'};
     }
 
     receiveAttack(x, y) {
         for (let hit of this.hits)
             if (hit.x == x && hit.y == y)
-                return;
-
-        for (let ship of this.ships)
-            for (let segment of ship.segments)
-                if (segment.x == x && segment.y == y)
-                    segment.isHit = true;
+                return {status: 'failure', message: 'square already hit'};
 
         this.hits.push({x, y});
+
+        for (let ship of this.ships) {
+            for (let segment of ship.segments) {
+                if (segment.x == x && segment.y == y) {
+                    segment.isHit = true;
+                    if (!ship.isSunk())
+                        return {status: 'success', result: 'enemy ship hit'};
+                    else if (this.areShipsSunk())
+                        return {status: 'success', result: 'all enemy ships sunk'};
+                    else
+                        return {status: 'success', result: 'enemy ship sunk'};
+                }
+            }
+        }
+
+        return {status: 'success', result: 'empty square'};
     }
 
     areShipsSunk() {
--- src/tests/gameboard.test.js
+++ src/tests/gameboard.test.js
@@ -6,48 +6,77 @@ beforeEach(() => {gameboard = new Gameboard()});
 
 test('add ship at coordinates', () => {
     let ship = new Ship(4, 4, 2, 'vertical');
-    gameboard.addShip(4, 4, 2, 'vertical');
+    expect(gameboard.addShip(4, 4, 2, 'vertical')).toEqual({status: 'success'});
     expect(gameboard.ships[0]).toEqual(ship);
 })
 
 test('reject ship placed out of bounds', () => {
-    gameboard.addShip(11, 11, 1, 'vertical');
+    expect(gameboard.addShip(11, 11, 1, 'vertical')).toEqual({
+        status: 'failure',
+        message: 'ship out of bounds'
+    });
     expect(gameboard.ships[0]).toBeUndefined();
 })
 
 test('reject ship that extends out of bounds', () => {
-    gameboard.addShip(9, 9, 2, 'vertical');
-    expect(gameboard.ships[0]).toBeUndefined();
-    gameboard.addShip(9, 9, 1, 'vertical');
-    expect(gameboard.ships[0]).toBeDefined();
+    expect(gameboard.addShip(9, 9, 2, 'vertical')).toEqual({
+        status: 'failure',
+        message: 'ship extends out of bounds'
+    });
+    expect(gameboard.addShip(9, 9, 1, 'vertical')).toEqual({status: 'success'});
 })
 
 test('reject ship overlapped with another ship', () => {
-    gameboard.addShip(4, 4, 2, 'vertical');
-    gameboard.addShip(4, 5, 2, 'vertical');
-    expect(gameboard.ships[1]).toBeUndefined();
-    gameboard.addShip(4, 6, 2, 'vertical');
-    expect(gameboard.ships[1]).toBeDefined();
+    expect(gameboard.addShip(4, 4, 2, 'vertical')).toEqual({status: 'success'});
+    expect(gameboard.addShip(4, 5, 2, 'vertical')).toEqual({
+        status: 'failure',
+        message: 'ship intersects other ship'
+    });
+    expect(gameboard.addShip(4, 6, 2, 'vertical')).toEqual({status: 'success'});
 })
 
 test('receive attack on empty square', () => {
-    gameboard.receiveAttack(4, 4);
+    expect(gameboard.receiveAttack(4, 4)).toEqual({status: 'success', result: 'empty square'});
     expect(gameboard.hits[0]).toEqual({x: 4, y: 4});
-    expect(gameboard.hits.length).toBe(1);
 })
 
 test('receive two attacks on same square', () => {
-    gameboard.receiveAttack(4, 4);
-    expect(gameboard.hits[0]).toEqual({x: 4, y: 4});
-    gameboard.receiveAttack(4, 4);
+    expect(gameboard.receiveAttack(4, 4)).toEqual({
+        status: 'success',
+        result: 'empty square'
+    });
+    expect(gameboard.receiveAttack(4, 4)).toEqual({
+        status: 'failure',
+        message: 'square already hit'
+    });
     expect(gameboard.hits.length).toBe(1);
 })
 
 test('receive attack on ship', () => {
     gameboard.addShip(4, 4, 2, 'vertical');
-    gameboard.receiveAttack(4, 3);
+    expect(gameboard.receiveAttack(4, 3)).toEqual({
+        status: 'success',
+        result: 'empty square'
+    });
     expect(gameboard.ships[0].segments[1].isHit).toBe(false);
-    gameboard.receiveAttack(4, 5);
+    expect(gameboard.receiveAttack(4, 5)).toEqual({
+        status: 'success',
+        result: 'enemy ship hit'
+    });
+    expect(gameboard.ships[0].segments[1].isHit).toBe(true);
+})
+
+test('report when ship has sunk', () => {
+    gameboard.addShip(4, 4, 2, 'vertical');
+    gameboard.addShip(8, 8, 2, 'vertical');
+    expect(gameboard.receiveAttack(4, 4)).toEqual({
+        status: 'success',
+        result: 'enemy ship hit'
+    });
+    expect(gameboard.receiveAttack(4, 5)).toEqual({
+        status: 'success',
+        result: 'enemy ship sunk'
+    });
     expect(gameboard.ships[0].segments[1].isHit).toBe(true);
 })
 
@@ -55,9 +84,13 @@ test('report when all sinks are sunk', () => {
     gameboard.addShip(4, 4, 2, 'vertical');
     gameboard.addShip(8, 4, 2, 'vertical');
     gameboard.receiveAttack(4, 4);
-    gameboard.receiveAttack(4, 5);
+    expect(gameboard.receiveAttack(4, 5)).toEqual({
+        status: 'success',
+        result: 'enemy ship sunk'
+    });
     gameboard.receiveAttack(8, 4);
-    expect(gameboard.areShipsSunk()).toBe(false);
-    gameboard.receiveAttack(8, 5);
-    expect(gameboard.areShipsSunk()).toBe(true);
+    expect(gameboard.receiveAttack(8, 5)).toEqual({
+        status: 'success',
+        result: 'all enemy ships sunk'
+    });
 })
