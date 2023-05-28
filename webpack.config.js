0ad890bd4a1556d34c21527149e8b3fbaa066604
Mateen <zahidx297@yahoo.com>
2023-05-28 05:22:31 -0400

    fix webpack asset naming

--- webpack.config.js
+++ webpack.config.js
@@ -9,13 +9,18 @@ module.exports = {
         filename: '[name].js',
         path: path.resolve(__dirname, 'dist'),
         clean: true,
+        assetModuleFilename: '[name][ext]',
     },
     module: {
         rules: [
             {
                 test: /\.css$/i,
                 use: ['style-loader', 'css-loader'],
-            }
+            },
+            {
+                test: /\.(png|svg|jpg|jpeg|gif)$/i,
+                type: 'asset/resource',
+            },
         ],
     },
     plugins: [
