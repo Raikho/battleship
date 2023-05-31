446de85bf1ded9786daa2336d764eaffe46c4218
Mateen <zahidx297@yahoo.com>
2023-05-31 06:15:47 -0400

    add a player test

--- src/tests/player.test.js
+++ src/tests/player.test.js
@@ -4,3 +4,11 @@ test('player initial test', () => {
     let player = new Player();
     expect(player.board).toBeDefined();
 });
+
+test('generate models', () => {
+    let player = new Player();
+    player.generateModels();
+    for (let model of player.board.models) {
+        expect(model).toBeDefined();
+    }
+});
